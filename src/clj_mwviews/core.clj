a3decbf8b1e4cc1448714d510f20c172c7dc0f62
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-01-21 17:28:04 -0800

    Add aggregate and top endpoints

--- src/clj_mwviews/core.clj
+++ src/clj_mwviews/core.clj
@@ -18,15 +18,26 @@
 (defn article-views-url [project access agent article granularity start end]
   (clojure.string/join "/" [root-url "per-article" project access agent article granularity start end]))
 
-(defn format-article-views [lazymap]
-  lazymap)
+(defn project-views-url [project access agent granularity start end]
+  (clojure.string/join "/" [root-url "aggregate" project access agent granularity start end]))
 
-(defn article-views [project articles & {:keys [access agent granularity start end]
+(defn top-views-url [project access year month day]
+  (clojure.string/join "/" [root-url "top" project access year month day]))
+
+
+(defn format-article-views [results]
+  results)
+
+(defn format-project-views [results]
+  results)
+
+(defn format-top-views [results]
+  results)
+
+(defn article-views [project articles start end & {:keys [access agent granularity]
                                         :or {access "all-access"
                                              agent "all-agents"
-                                             granularity "daily"
-                                             start "20150911"
-                                             end "20150913"}}]
+                                             granularity "daily"}}]
   (let [start-date (format-date granularity start)
         end-date (format-date granularity end)]
     (format-article-views
@@ -34,6 +45,24 @@
              (article-views-url project access agent % granularity start-date end-date))
            articles))))
 
-(article-views "en.wikipedia" ["Barack_Obama" "Apple"])
+(defn project-views [projects start end & {:keys [access agent granularity]
+                                           :or {access "all-access"
+                                                agent "all-agents"
+                                                granularity "daily"}}]
+  (let [start-date (format-date granularity start)
+        end-date (format-date granularity end)]
+    (format-project-views
+      (map #(get-json
+             (project-views-url % access agent granularity start-date end-date))
+           projects))))
 
+(defn top-articles [project year month day & {:keys [access]
+                                              :or {access "all-access"}}]
+  (format-top-views
+    (get-json (top-views-url project access year month day))))
 
+;(article-views "en.wikipedia" ["Barack_Obama" "Apple"] "20150911" "20150913")
+
+;(project-views ["en.wikipedia" "de.wikipedia"] "20150911" "20150913")
+
+;(top-articles "en.wikipedia" "2015" "09" "10")
