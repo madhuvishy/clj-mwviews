a5c74289e3f7995c4140263205d1e7ee020798f4
Madhumitha Viswanathan <mviswanathan@wikimedia.org>
2016-01-31 20:37:58 -0800

    Better docs

--- README.md
+++ README.md
@@ -4,5 +4,30 @@ A Clojure client library for the Wikimedia Pageviews API
 
 ## Usage
 
-FIXME
+```
+; Article view counts
+(article-views "en.wikipedia" ["Barack_Obama" "Apple"] "20150911" "20150913")
+(article-views "en.wikipedia" ["Barack_Obama" "Apple"] "20150911" "20150913" :access "mobile app")
+
+
+; Per-project aggregate view counts
+(project-views ["en.wikipedia" "de.wikipedia"] "20150911" "20150913")
+(project-views ["en.wikipedia" "de.wikipedia"] "20150911" "20150913" :agent "user")
+
+
+; Top articles view counts
+(top-articles "en.wikipedia" "2015" "09" "10")
+
+```
+
+## API Documentation
+
+[https://wikitech.wikimedia.org/wiki/Analytics/PageviewAPI](Wikitech)
+[https://wikimedia.org/api/rest_v1/?doc](Technical docs)
+
+## TODO
+
+* Package library as jar
+* Easier methods to access api last-n-days style
+* Explore different formats of output like producing a timeseries instead of the regular api json output
 
--- src/clj_mwviews/core.clj
+++ src/clj_mwviews/core.clj
@@ -1,13 +1,15 @@
 (ns clj-mwviews.core
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]
-            [clj-time.format :as f]))
+            [clj-time.format :as f]
+            [clj-time.core :as t]))
 
 (def root-url "https://wikimedia.org/api/rest_v1/metrics/pageviews")
 
 (defn get-json [url]
   (json/read-str (:body (client/get url))))
 
+; Datetime helpers
 (def hourly (f/formatter "yyyyMMddHH"))
 (def daily (f/formatter "yyyyMMdd"))
 (def yearly (f/formatter "yyyy"))
@@ -15,6 +17,7 @@
 (defn format-date [granularity date-string]
   (f/unparse hourly (f/parse (load-string granularity) date-string)))
 
+; Construct the API urls for the different endpoints
 (defn article-views-url [project access agent article granularity start end]
   (clojure.string/join "/" [root-url "per-article" project access agent article granularity start end]))
 
@@ -25,6 +28,8 @@
   (clojure.string/join "/" [root-url "top" project access year month day]))
 
 
+; These format methods are doing nothing right now - they are just placeholders
+; to replace with a different format for results.
 (defn format-article-views [results]
   results)
 
@@ -34,21 +39,26 @@
 (defn format-top-views [results]
   results)
 
-(defn article-views [project articles start end & {:keys [access agent granularity]
-                                        :or {access "all-access"
-                                             agent "all-agents"
-                                             granularity "daily"}}]
-  (let [start-date (format-date granularity start)
-        end-date (format-date granularity end)]
-    (format-article-views
-      (map #(get-json
-             (article-views-url project access agent % granularity start-date end-date))
-           articles))))
+
+(defn article-views
+  "Get view counts for given project and list of articles"
+  ([project articles start end & {:keys [access agent granularity]
+                                  :or   {access      "all-access"
+                                         agent       "all-agents"
+                                         granularity "daily"}}]
+   (let [start-date (format-date granularity start)
+         end-date (format-date granularity end)]
+     (format-article-views
+       (map #(get-json
+              (article-views-url project access agent % granularity start-date end-date))
+            articles)))))
+
 
 (defn project-views [projects start end & {:keys [access agent granularity]
                                            :or {access "all-access"
                                                 agent "all-agents"
                                                 granularity "daily"}}]
+  "Get aggregate view counts for given project"
   (let [start-date (format-date granularity start)
         end-date (format-date granularity end)]
     (format-project-views
@@ -58,11 +68,6 @@
 
 (defn top-articles [project year month day & {:keys [access]
                                               :or {access "all-access"}}]
+  "Get view counts for most viewed articles over a day"
   (format-top-views
     (get-json (top-views-url project access year month day))))
-
-;(article-views "en.wikipedia" ["Barack_Obama" "Apple"] "20150911" "20150913")
-
-;(project-views ["en.wikipedia" "de.wikipedia"] "20150911" "20150913")
-
-;(top-articles "en.wikipedia" "2015" "09" "10")
